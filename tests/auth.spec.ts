fb84ee1fdacb05ec99aded4287628d2d990ed7eb
Lincoln Neilsen <lincneilsen@gmail.com>
2026-02-07 19:50:49 -0700

    Login testing; jest removed

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,7 +1,8 @@
-import { test, expect } from 'playwright-test-coverage';
+import {test, expect} from 'playwright-test-coverage';
 
 //mock out all calls to the backend
 //to check things are being mocked out correctly change env.dev variable to bogus url
+const site = 'https://pizza.lincstores.click/';
 
 test('home page', async ({page}) => {
     await page.goto('/');
@@ -36,7 +37,7 @@ test('register', async ({page}) => {
         });
     });
     //User actions
-    await page.goto('https://pizza.lincstores.click/');
+    await page.goto(site);
     await page.getByRole('link', {name: 'Register'}).click();
     // Fill form
     await page.getByRole('textbox', {name: 'Full name'}).fill('James');
@@ -46,4 +47,34 @@ test('register', async ({page}) => {
 
     await expect(page.getByRole('link', {name: 'Logout'})).toBeVisible();
 });
+
+test('login', async ({page}) => {
+    await page.route('**/api/auth', async route => {
+
+        await route.fulfill({
+            status: 200,
+            contentType: 'application/json',
+            body: JSON.stringify({
+                user: {
+                    email: 't@jwt.com',
+                    name: 't',
+                },
+                token: 'fake-jwt-token',
+            }),
+        });
+    })
+
+
+//User action
+    await page.goto(site);
+    await page.getByRole('link', {name: 'Login'}).click();
+    await page.getByRole('textbox', {name: 'Email address'}).click();
+    await page.getByRole('textbox', {name: 'Email address'}).fill('t@jwt.com');
+    await page.getByRole('textbox', {name: 'Password'}).click();
+    await page.getByRole('textbox', {name: 'Password'}).fill('test');
+    await page.getByRole('button', {name: 'Login'}).click();
+    await page.pause();
+    await page.getByRole('link', {name: 't', exact: true}).click();
+    await expect(page.getByRole('main')).toContainText('t@jwt.com');
+})
 
